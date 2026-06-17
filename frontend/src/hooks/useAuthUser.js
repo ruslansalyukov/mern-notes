26d03c01bcc9ff0b99f82e1fa671f0437b0eaec3
Ruslan Salyukov <RuslanSalyukov1999588@gmail.com>
2026-06-17 12:24:51 +0300

    fix auth api

--- frontend/src/hooks/useAuthUser.js
+++ frontend/src/hooks/useAuthUser.js
@@ -2,10 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 import api from "../api/axios";
 
 export const useAuthUser = () => {
+
+	const API_URL = import.meta.env.VITE_API_URL
+
 	return useQuery({
 		queryKey: ['user'],
 		queryFn: async () => {
-			const res = await api.get('/auth/me');
+			const res = await api.get(API_URL);
 			return res.data
 		},
 		retry: false,
