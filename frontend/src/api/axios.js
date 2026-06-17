726de934358faa372cc7bb12d4a88cca89fe6b37
Ruslan Salyukov <RuslanSalyukov1999588@gmail.com>
2026-06-17 12:47:51 +0300

    fix api

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.VITE_API_URL
 
 const api = axios.create({
-	baseURL: BASE_URL,
+	baseURL: 'https://mern-notes-wine.vercel.app/api',
 	withCredentials: true,
 })
 
