bccecb1df79022b3d849d93a6f90723663a41ef5
Ruslan Salyukov <RuslanSalyukov1999588@gmail.com>
2026-06-17 13:07:43 +0300

    fix auth secure

--- backend/src/controllers/auth/authController.js
+++ backend/src/controllers/auth/authController.js
@@ -63,8 +63,8 @@ export const login = async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            sameSite: 'lax',
-            secure: process.env.NODE_ENV === 'production',
+            secure: true,
+            sameSite: 'none',
             maxAge: 7 * 24 * 60 * 60 * 1000,
         })
 
@@ -79,8 +79,8 @@ export const logout = async (req, res) => {
     try {
         res.clearCookie('token', {
             httpOnly: true,
-            sameSite: 'lax',
-            secure: process.env.NODE_ENV === 'production',
+            secure: true,
+            sameSite: 'none',
         });
         res.status(200).json({
             success: true,
