0ce26650fb02f7a2a57301213f4993b2cf2aa4e4
Ruslan Salyukov <RuslanSalyukov1999588@gmail.com>
2026-06-21 02:42:33 +0300

    updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
 		<>
 			<header className="sticky top-0 z-50">
 				{/* Desktop nav */}
-				<div className="pt-10 from-black inset-0 bg-linear-to-b backdrop-blur-sm">
+				<div className="pt-10 bg-black/15 backdrop-blur-sm">
 					<nav className="flex items-center justify-between gap-10 bg-white/2 border border-white/5 p-3 rounded-full font-jakarta relative z-50 ">
 						<a href="/" className="logo relative ml-2">Notes</a>
 						<div className="flex items-center gap-3">
